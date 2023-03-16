cd1553b0f2ef5441610fe1cd2693d2b8ea0d99ce
GithubKolden <122537835+GithubKolden@users.noreply.github.com>
2023-03-16 21:27:49 +0100

    design cart shop etc...
    ...

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -5,6 +5,8 @@ import { TiDeleteOutline } from 'react-icons/ti';
 import { useStateContext } from '../../context/StateContext';
 import { urlFor } from '../../lib/client';
 //import getStripe from '../lib/getStripe'; 
+import { Box, Container } from '@mui/system';
+import { Button } from '@mui/material';
 
 const Cart = () => {
   const cartRef = useRef();
@@ -12,77 +14,93 @@ const Cart = () => {
   /*HANDLECHECKOUT HER!*/
 
   return (
-    <div className="cart-wrapper" ref={cartRef} style={{width: "100%", height: "20px", paddingTop: "3%"}}>
-      <div className="cart-container">
+    <Container
+    style={{
+      marginTop: '50px',
+      color: '#494949',
+      maxWidth: '525px',
+      maxHeight: '40vh',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+      <div className="cart-wrapper" ref={cartRef} style={{width: "100%", height: "20px", paddingTop: "3%"}}>
+        <div className="cart-container">
 
-        {cartItems.length < 1 && (  
-          <div className="empty-cart">
+          {cartItems.length < 1 && (  
+            <div className="empty-cart">
+              <AiOutlineShopping size={150} />
+              <h3>Your shopping bag is empty</h3>
+              <Link href="/shop">
+                  Continue Shopping
+              </Link>
 
-            <AiOutlineShopping size={150} />
-            <h3>Your shopping bag is empty</h3>
-            <Link href="/shop">
-                Continue Shopping
-            </Link>
+            </div>
+          )}
 
-          </div>
-        )}
+          <div className="product-container">
 
-        <div className="product-container">
+            {cartItems.length >= 1 && cartItems.map((item) => (
+              
+              <div className="product" key={item._id}>
 
-          {cartItems.length >= 1 && cartItems.map((item) => (
+                <img src={urlFor(item?.image[0])} className="cart-product-image" />
 
-            <div className="product" key={item._id}>
+                <div className="item-desc">
 
-              <img src={urlFor(item?.image[0])} className="cart-product-image" />
-
-              <div className="item-desc">
-
-                <div className="flex top">
-                  <h5>{item.name}</h5>
-                  <h4>${item.price}</h4>
-                </div>
-
-                <div className="flex bottom">
-
-                  <div>
-                  <p className="quantity-desc">
-                   <span className="minus" onClick={() => toggleCartItemQuanitity(item._id, 'dec') }>
-                    <AiOutlineMinus />
-                    </span>
-                    <span className="num" onClick="">{item.quantity}</span>
-                    <span className="plus" onClick={() => toggleCartItemQuanitity(item._id, 'inc') }><AiOutlinePlus /></span>
-                  </p>
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>${item.price}</h4>
                   </div>
 
-                  <button type="button" className="remove-item" onClick={() => onRemove(item)}>
-                    <TiDeleteOutline />
-                  </button>
+                  <div className="flex bottom">
+
+                    <div>
+                    <p className="quantity-desc">
+                    <span className="minus" onClick={() => toggleCartItemQuanitity(item._id, 'dec') }>
+                      <AiOutlineMinus />
+                      </span>
+                      <span className="num" onClick="">{item.quantity}</span>
+                      <span className="plus" onClick={() => toggleCartItemQuanitity(item._id, 'inc') }><AiOutlinePlus /></span>
+                    </p>
+                    </div>
+
+                    <button type="button" className="remove-item" onClick={() => onRemove(item)}>
+                      <TiDeleteOutline style={{color: "red"}} />
+                    </button>
+
+                  </div>
 
                 </div>
+              </div>
 
+            ))}
+
+          </div>
+
+          <div class="drawed_line_total"></div>
+
+          {cartItems.length >= 1 && (
+            <div className="cart-bottom">
+              <div className="total">
+                <h3>Subtotal:</h3>
+                <h3>{totalPrice},-</h3>
+              </div>
+              <div style={{textAlign: "center"}}>
+                <Button
+                variant="contained"
+                color="success"
+                type="button"
+                style={{ cursor: 'pointer' }}>
+                  Continue to payment
+                </Button>
               </div>
             </div>
-
-          ))}
+          )}
 
         </div>
-
-        {cartItems.length >= 1 && (
-          <div className="cart-bottom">
-            <div className="total">
-              <h3>Subtotal:</h3>
-              <h3>${totalPrice}</h3>
-            </div>
-            <div className="btn-container">
-              <button type="button" className="btn">
-                Continue to payment
-              </button>
-            </div>
-          </div>
-        )}
-
       </div>
-    </div>
+    </Container>
   )
 }
 
--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -2,10 +2,21 @@ import { useState, useEffect } from "react";
 import { client, urlFor } from '../../lib/client';
 import Link from "next/link";
 import { TextField, Box, Container, Grid, ButtonGroup, Button } from "@mui/material";
+import { useStateContext } from '../../context/StateContext';
+import NoSsr from '@mui/material/NoSsr';
 
 const Shop = ({ products }) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredProducts, setFilteredProducts] = useState(products);
+  const { image, name, details, price, InStock , ferskhet} = products;
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+  const [isInStock, setIsInStock] = useState(InStock);
+
+  const handleAddToCart = () => {
+    if (isInStock) {
+      onAdd(product, qty);
+    }
+  };
 
   const handleFilter = (filterTerm) => {
     if (filterTerm === "All") {
@@ -17,18 +28,32 @@ const Shop = ({ products }) => {
 
   return (
     <div>
-      <Container direction="column" justify="center" style={{borderStyle: "none", paddingTop: "1%"}}>
+      <Container direction="column" justify="center" style={{borderStyle: "none", paddingTop: "0.5%"}}>
+
         <Grid container direction="column" alignItems="center" justify="center" style={{borderStyle: "none"}}>
           <TextField sx={{ my: 5 }} type="search" id="outlined-basic" label="Search" placeholder="Productname" variant="outlined" aria-label="Search" aria-describedby="search-addon" onChange={event => setSearchTerm(event.target.value)} />
         </Grid>
-        <Grid container style={{borderStyle: "none"}} justifyContent="center">
-          <ButtonGroup variant="contained" aria-label="outlined primary button group">
+
+        <Grid container style={{borderStyle: "none", paddingTop: "0.5%"}}>
+
+          
+          <Grid item alignItems="center" md={4} style={{textAlign: "center", borderStyle: "none"}}>
             <Button onClick={() => handleFilter("All")}>All</Button>
-            <Button onClick={() => handleFilter("fersk")}>Fersk</Button>
-            <Button onClick={() => handleFilter("tørket")}>Tørket</Button>
-          </ButtonGroup>
-        </Grid>
-        <Grid container style={{borderStyle: "none"}}>
+            <div class="drawed_line_item"></div>
+          </Grid>
+          
+          <Grid item alignItems="center" md={4} style={{textAlign: "center", borderStyle: "none"}}>
+            <Button onClick={() => handleFilter("fersk")}>Fresh</Button>
+            <div class="drawed_line_item"></div>
+          </Grid>
+          
+          <Grid item alignItems="center" md={4} style={{textAlign: "center", borderStyle: "none"}}>
+            <Button onClick={() => handleFilter("tørket")}>Dried</Button>
+            <div class="drawed_line_item"></div>
+          </Grid>
+          
+          
+
           {filteredProducts
             .filter((product)=>{
               if (searchTerm === "") { return product.InStock === true }
@@ -38,20 +63,37 @@ const Shop = ({ products }) => {
               return (
                 <Grid key={product.slug.current} item alignItems="center" md={4} style={{textAlign: "center", borderStyle: "none"}}>
                   <Link key={product.slug.current} style={{ cursor: "pointer", textDecoration: "none" }} href={`/product/${product.slug.current}`}>
+
                     <div className="card card-1">
-                      <div className="top">
-                        <Box key={product.slug.current} sx={{ width: "100%" }} component="img" src={urlFor(product.image && product.image[0]).url()} alt={product.slug.current}/>
-                      </div>
-                      <div className="bottom">
-                        <p>{product.name}</p>
-                        <input placeholder="YO!"/>
-                      </div> 
+
+                      
+                        <div className="top">
+                          <Box key={product.slug.current} sx={{ width: "100%" }} component="img" src={urlFor(product.image && product.image[0]).url()} alt={product.slug.current}/>
+                        </div>
+                      
+
+                        <div className="bottom">
+                          <p>
+                            {product.name}
+                          </p>
+                        </div>
+
                     </div>
                   </Link>
+
+                  <NoSsr>
+                    <div className="Buttons" style={{marginTop: "-3%", marginBottom: "4%"}}>
+                      <Button variant="contained" color="success" type="button" style={{ cursor: 'pointer' }} onClick={handleAddToCart}>
+                        ADD TO CART
+                      </Button>
+                    </div>
+                  </NoSsr>
+                  
                 </Grid>
               )
             })}
         </Grid>
+        
       </Container>
     </div>
   )
@@ -66,3 +108,23 @@ export const getServerSideProps = async () => {
   }
 }
 export default Shop;
+
+/*
+<Grid container style={{borderStyle: "none"}} justifyContent="center">
+<ButtonGroup variant="contained" aria-label="outlined primary button group">
+
+<Grid item alignItems="center" md={4} style={{textAlign: "center", borderStyle: "none"}}>
+  <Button onClick={() => handleFilter("All")}>All</Button>
+</Grid>
+
+<Grid item alignItems="center" md={4} style={{textAlign: "center", borderStyle: "none"}}>
+  <Button onClick={() => handleFilter("fersk")}>Fersk</Button>
+</Grid>
+
+<Grid item alignItems="center" md={4} style={{textAlign: "center", borderStyle: "none"}}>
+  <Button onClick={() => handleFilter("tørket")}>Tørket</Button>
+</Grid>
+
+</ButtonGroup>
+</Grid>
+*/
