b532562fff4a14e6ab5213caa7c76ccfde919c8b
Haiahm <haiderahmed641@gmail.com>
2023-03-22 12:46:06 +0100

    About/index
    About siden er nesten good og home page mangler kun riktige bilder

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,44 @@
+import React from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+
+
 const About = () => {
   return (
-    <div>
-      <p>ABOUT</p>
-    </div>
-  )
-}
+  
+    <Grid container>
+      <Grid item xs={12}>
+        <Box
+          sx={{
+            backgroundImage: 'url("https://www.pngmagic.com/product_images/eCommerce-website-banner-background.jpg")',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <img src="https://melkoghonning.no/wp-content/uploads/2020/09/carousel-hovedbilde.jpg" alt="Your Image" style={{ maxWidth: '100%', marginTop: "10%", marginLeft: "1%" }} />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography variant="h4" gutterBottom marginTop={'10%'} textAlign={'center'}>
+          About Our Website
+        </Typography>
+        <Typography variant="body1" textAlign={'center'}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis urna ut ligula consequat luctus. 
+          Aenean in magna nec risus malesuada ultricies. Sed ut justo libero. 
+          Nam sagittis nisl ut velit pretium, a semper orci feugiat. Sed lacinia lobortis faucibus. 
+          Sed luctus interdum aliquam. 
+        </Typography>
+      </Grid>
+    </Grid>
+    </Box>
+    </Grid>
+    </Grid>
+    
+  );
+};
+
 export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,40 @@
 import Image from 'next/image'
-import logo from './img/LOGO.png';
+import LOGO from './img/LOGO.png';
 import { Grid, Box } from '@mui/material';
 
 const Home = () => {
-  return (  
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      style={{ minHeight: '100vh' }}
-    >
-
-      <Grid item xs={12} sx={{ textAlign: "center" }}>
-        <Box sx={{ maxWidth: '100%', height: 'auto', mx: "auto" }}>
-          <Image src={logo} alt="" className="img-fluid" layout="responsive" />
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Box
+          sx={{
+            backgroundImage: 'url("https://www.pngmagic.com/product_images/eCommerce-website-banner-background.jpg")',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: '0 10%', // responsive padding
+            textAlign: 'center', // center align text on small screens
+            '@media screen and (max-width: 600px)': { // apply styles for small screens
+              padding: '0 5%',
+              height: 'auto',
+            },
+          }}
+        >
+          <Image
+            src={LOGO}
+            alt="Logo"
+            width={700}
+            height={200}
+            style={{ maxWidth: '100%', height: 'auto' }} // make image responsive
+          />
         </Box>
       </Grid>   
    
     </Grid> 
   )
-};
+}
+
 export default Home;
