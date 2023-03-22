4157bc3e5424d4f2bd2a2ff37a1e10625b20cb34
Kapteinen <90222321+Kapteinen@users.noreply.github.com>
2023-03-22 15:03:20 +0100

    contactme <3 bby
    Am i clear?!?!?

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,30 +5,36 @@ const Contact = () => {
   return (
     <Container>
   <Grid container justifyContent='center'>
-    <Box sx={{ display: 'flex', justifyContent: 'center', borderStyle: "solid" }}>
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
       <h1>Contact</h1>
     </Box>
   </Grid>
   <Grid container justifyContent='center' spacing={2}>
     <Grid item xs={12} sm={6}>
-      <Box sx={{ display: 'flex', justifyContent: 'center', borderStyle: "dotted", position: 'relative'}}>
-        <img src='https://images.squarespace-cdn.com/content/v1/5b4606ec96d4557cbd0f1e5d/1613862613056-VMVHMPY7YDAJ8GPXNAR6/Carousel_Maren-Mjelstad.jpg?format=1500w' alt='' style={{ maxWidth: '100%'}} />
+      <Box sx={{ display: 'flex', justifyContent: 'center', borderStyle: "solid", position: 'relative'}}>
+        <img src='https://www.1800flowers.com/blog/wp-content/uploads/2017/03/single-red-rose.jpg' alt='' style={{ maxWidth: '100%'}} />
       </Box>
     </Grid>
     <Grid item xs={12} sm={6}>
-      <Box sx={{ justifyContent: 'center', borderStyle: "dotted" }}>
+      <Box sx={{ justifyContent: 'center', borderStyle: "solid" }}>
         <Typography variant='h4' gutterBottom textAlign={'center'}>
           Contact information
         </Typography>
-        <Typography variant = 'body1' textAlign={'center'}>
-          yo
+        <Typography variant = 'body1' textAlign={'center'} style={{textDecoration: 'underline', marginTop: '2rem'}}>
+          E-post: carousel.oslo@hotmail.com
+        </Typography>
+        <Typography variant = 'body1' textAlign={'center'} style={{textDecoration: 'underline', marginTop: '1rem'}}>
+         Tlf: 12345678
+        </Typography>
+        <Typography variant = 'body1' textAlign= {'center'} style={{textDecoration: 'underline', marginTop: '1rem'}}>
+         Adresse: 1621 Gressvik, Hurrødåsen 10
         </Typography>
       </Box>
     </Grid>
   </Grid>
   <Grid container justifyContent='center' spacing={2}>
     <Grid item xs={12} sm={6}>
-      <Box sx={{ justifyContent: 'center', borderStyle: "dotted" }}>
+      <Box sx={{ justifyContent: 'center', borderStyle: "solid" }}>
         <Typography variant='h4' gutterBottom textAlign={'center'}>
           Contact information
         </Typography>
@@ -38,8 +44,8 @@ const Contact = () => {
       </Box>
     </Grid>
     <Grid item xs={12} sm={6}>
-      <Box sx={{ display: 'flex', justifyContent: 'center', borderStyle: "dotted" }}>
-        <img src='https://images.squarespace-cdn.com/content/v1/5b4606ec96d4557cbd0f1e5d/1613862613056-VMVHMPY7YDAJ8GPXNAR6/Carousel_Maren-Mjelstad.jpg?format=1500w' alt='' style={{ maxWidth: '100%'}} />
+      <Box sx={{ display: 'flex', justifyContent: 'center', borderStyle: "solid" }}>
+        <img src='https://www.gardeningknowhow.com/wp-content/uploads/2019/09/flower-color.jpg' alt='' style={{ maxWidth: '100%'}} />
       </Box>
     </Grid>
   </Grid>
