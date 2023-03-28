271dc6c77d8dbb09dfc369863a9df772c11590a3
protovinh <vinvu2000@hotmail.com>
2023-03-28 22:55:01 +0200

    fixed About Page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,52 +1,48 @@
 import { Container, Grid, Box, Typography } from '@mui/material';
 import { client, urlFor } from '../../lib/client';
+import Background from "./background"
 
-const About = ({abouts}) => {
-//const { image, contact, email, telefon, adresse} = abouts;
-//console.log(abouts[0].adresse)
-  
+const About = ({ abouts, backgrounds }) => {
   return (
-    <Container id="responsive-container-about">
-      
-      <Grid container justifyContent='center'>
-        <Box sx={{ display: 'flex', justifyContent: 'center', paddingBottom: "2%"}}>
-          <h1>About</h1>
-        </Box>
-      </Grid>
-      <Grid container justifyContent='center' spacing={2} style={{ border: 'none', color: 'red', position: 'relative'}}>
-        <Grid item xs={12} sm={6}>
-          {abouts?.map((item, index) =>(
-            <Box 
-            component="img"
-            src={urlFor(item.image[0])}
-            alt="about images"
-            key={item.image[0]}
-            style={{widht: "10vh", height:"60vh"}}
-            >
-          </Box>
+    <>
+      <Background backgrounds={backgrounds} style={{ zIndex: -1 }} />
+      <Box sx={{ position: 'absolute', top: '70px', left: 0, width: '100%', display: 'flex', justifyContent: 'center', zIndex: 1 }}>
+        <Typography variant='h2' gutterBottom>About</Typography>
+      </Box>
+      <Container sx={{ position: 'relative', display: 'flex', justifyContent: 'center', marginTop: '150px' }}>
+        <Box sx={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(255, 255, 255, 0.7)', backdropFilter: 'blur(5px)', zIndex: -1 }} />
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', padding: '2rem', backgroundColor: 'rgba(255, 255, 255, 0.7)', backdropFilter: 'blur(5px)', borderRadius: '20px', zIndex: 0 }}>
+          {abouts?.map((item, index) => (
+            <Grid container justifyContent='center' alignItems='center' key={item.slug.current}>
+              <Grid item xs={12} sm={6} order={{ xs: 2, sm: 1 }}>
+                <Box
+                  component="img"
+                  src={urlFor(item.image[0])}
+                  alt="about images"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} order={{ xs: 1, sm: 2 }}>
+                <Box id="contact-box-1" sx={{ justifyContent: 'center', borderStyle: "none" }}>
+                  <Typography variant='h4' gutterBottom textAlign={'center'}>
+                    {item.contact}
+                  </Typography>
+                  <Typography variant='body1' textAlign={'center'} style={{ textDecoration: 'underline', marginTop: '1rem' }}>
+                    {item.email}
+                  </Typography>
+                  <Typography variant='body1' textAlign={'center'} style={{ textDecoration: 'underline', marginTop: '1rem' }}>
+                    {item.telefon}
+                  </Typography>
+                  <Typography variant='body1' textAlign={'center'} style={{ textDecoration: 'underline', marginTop: '1rem' }}>
+                    {item.adresse}
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
           ))}
-          <Grid item xs={12} sm={6}>
-          <Box id="contact-box-1" sx={{ justifyContent: 'center', borderStyle: "none" }}>
-            <Typography variant='h4' gutterBottom textAlign={'center'}>
-              {abouts[0].contact}
-            </Typography>
-            <Typography variant = 'body1' textAlign={'center'} style={{textDecoration: 'underline', marginTop: '1rem'}}>
-              {abouts[0].email}
-            </Typography>
-            <Typography variant = 'body1' textAlign={'center'} style={{textDecoration: 'underline', marginTop: '1rem'}}>
-             {abouts[0].telefon}
-            </Typography>
-            <Typography variant = 'body1' textAlign= {'center'} style={{textDecoration: 'underline', marginTop: '1rem'}}>
-              {abouts[0].adresse}
-            </Typography>
-          </Box>
-        </Grid>
-          
-        </Grid>
-        
-
-      </Grid>
-    </Container>
+        </Box>
+      </Container>
+    </>
   );
 }
 
@@ -54,8 +50,12 @@ export const getServerSideProps = async () => {
   const AboutQuery = `*[_type == "about"] { image, contact, email, telefon, adresse, slug,  }`;
   const abouts = await client.fetch(AboutQuery);
 
+  const backgroundsQuery = `*[_type == "background"] { image, name, slug }`;
+  const backgrounds = await client.fetch(backgroundsQuery)
+
   return {
-    props: { abouts, }
+    props: { abouts, backgrounds }
   }
 }
+
 export default About;
